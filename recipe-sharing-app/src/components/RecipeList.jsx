1edda933142c24e95fe88804f4320c50f4ea735a
desthedeveloper <161512174+desthedeveloper@users.noreply.github.com>
2024-09-16 08:05:20 +0100

    Add files via upload
    added a new file

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,23 @@
+// src/components/RecipeList.jsx
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  // Get the recipes from the store
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          {/* Add a Link to the recipe details page */}
+          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
